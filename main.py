861ab661d957ec549a7a295aee76351dc5f1f08e
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2021-12-02 19:14:17 +0100

    made some changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium.webdriver import ChromeOptions
 from selenium import webdriver
 from util.common import title
+from util.login import login
 from time import sleep
 from lxml import html
 import requests
@@ -45,43 +46,12 @@ def download_chromedriver():
         os.remove(latest_driver_zip)
 
 
-def loaded(driver, xpath, xpath1=None):
-    try:
-        elm = driver.find_element_by_xpath(xpath)
-        return elm
-    except Exception as e:
-        try:
-            if xpath1 != None:
-                elm = driver.find_element_by_xpath(xpath1)
-                return elm
-        except:
-            pass
-        sleep(1)
-        loaded(xpath)
-
-
 def main():
     driver = webdriver.Chrome()
     if multiAcc == True:
         for user in usernames:
             url = "https://github.com/{}".format(user)
-            driver.get(url)
-            driver.find_element_by_xpath(
-                '//*[@id="js-pjax-container"]/div[1]/div/div/div[2]/div/nav/a[2]').click()
-            tree = html.fromstring(driver.page_source)
-
-            for product_tree in tree.xpath('//*[@id="user-repositories-list"]/ul'):
-                titles = product_tree.xpath(
-                    '//a[@itemprop="name codeRepository"]/text()')
-                for title in titles:
-                    title = str(title).replace(" ", "").replace("\n", "")
-                    print(title)
-                    driver.get(url + f"/{title}")
-
-                    star = loaded(driver,
-                                  '//button[contains(@data-ga-click, "text:Star")]',
-                                  '//a[@aria-label="You must be signed in to star a repository"]')
-                    star.click()
+            login(driver, url)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from selenium.webdriver import ChromeOptions
 from selenium import webdriver
 from time import sleep
 from lxml import html
+import json
 
 
 headless = False
@@ -20,6 +21,20 @@ sleep(1)
 tree = html.fromstring(driver.page_source)
 
 
+def get_config():
+    global username, usernames, multiAcc, login_username, login_password
+    with open("./config.json") as f:
+        config = json.load(f)
+
+        multiAcc = config["Multible-Accounts"]
+        if str(multiAcc) == "true":
+            multiAcc = True
+            usernames = config["Github-Usernames"]
+        username = config["Github-Username"]
+        login_username = config["logins"]["username"]
+        login_password = config["logins"]["password"]
+
+
 def loaded(xpath, xpath1=None):
     try:
         elm = driver.find_element_by_xpath(xpath)
@@ -35,6 +50,7 @@ def loaded(xpath, xpath1=None):
         loaded(xpath)
 
 
+get_config()
 for product_tree in tree.xpath('//*[@id="user-repositories-list"]/ul'):
     titles = product_tree.xpath('//a[@itemprop="name codeRepository"]/text()')
     for title in titles:
@@ -45,3 +61,26 @@ for product_tree in tree.xpath('//*[@id="user-repositories-list"]/ul'):
         star = loaded('//button[contains(@data-ga-click, "text:Star")]',
                       '//a[@aria-label="You must be signed in to star a repository"]')
         star.click()
+
+        username = driver.find_element_by_xpath('//*[@id="login_field"]')
+        password = driver.find_element_by_xpath('//*[@id="password"]')
+
+        username.clear()
+        username.send_keys(login_username)
+
+        password.clear()
+        password.send_keys(login_password)
+
+        driver.find_element_by_xpath(
+            '//*[@id="login"]/div[4]/form/div/input[12]').click()
+        sleep(1)
+        try:
+            driver.find_element_by_xpath(
+                '//*[@id="js-flash-container"]/div/div')
+            sleep(1000000)
+        except:
+            pass
+        sleep(2)
+
+# "username": "YOUR USERNAME/EMAIL FOR GITHUB",
+# "password": "YOUR GITHUB PASSWORD (needed for login to star repo)"
